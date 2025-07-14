bcb12a72fe98aed62ea885d70312faed7cdfbb51
paryaarezoo <paryaarezoo@gmail.com>
2025-07-15 00:52:35 +0330

    Reorganized project folder structure and fixed broken asset paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html", "./contactUs.html", "./src/**/*.{js,html}"],
+  content: [
+    "./index.html",
+    "**/*.html", 
+    "./assets/**/*.{js}", 
+  ],
   theme: {
     extend: {
       colors: {
-
         accent: "#00a4b0",
-
         primary: {
           DEFAULT: "#f78f9b",
           hover: "#faadb6",
         },
-      }
+      },
     },
   },
-  plugins: [
-
-  ],
-  "template": "static"
-
-}
+  plugins: [],
+};
